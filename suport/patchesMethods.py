e00c46173f86b56376fdb17bd0f08f097b8333be
Luciano <lucianoterres@gmail.com>
2024-08-24 09:49:15 -0300

    pm locals only

--- suport/patchesMethods.py
+++ suport/patchesMethods.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import locals
 
 # Rotaciona a imagem em torno do centro
 def rotateImage(img, angle):
@@ -16,24 +17,21 @@ def rotateImage(img, angle):
 
 def loadDataBase(samples=200, treshold=100):
     #define localização dos diretórios de imagens
-    inHouse=False
-    inES0004605=True
-    inNote=False
-   
-    if inHouse:
+  
+    if locals.inHouse:
         #Desktop I3
         housepath=r'D:\_0Luciano\_0PHD\datasets\tgs-sal'
         TRAIN_CSV = housepath + r'\train1090.csv'
         IMAGES_DIR = housepath + r'\train\images'
         MASK_DIR = housepath + r'\masks10-90'
     
-    if inNote:
+    if locals.inNote:
         notePath = r'D:\\_phd\datasets\\tgsSalt\\'
         TRAIN_CSV = notePath + r'train1090.csv'
         IMAGES_DIR = notePath + r'train\\images'
         MASK_DIR =  notePath + r'train\\masks'
     
-    if inES0004605:
+    if locals.inES0004605:
         esPath = r'G:\\_phd\dataset\\tgs-salt\\'
         TRAIN_CSV = esPath + 'saltMaskOk.csv'
         IMAGES_DIR= esPath + r'train\\images' 
