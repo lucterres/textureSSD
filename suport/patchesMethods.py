c80ed90cb7a5a9335a2cfdc2c9d12dabddaeac2f
Luciano Terres <45270459+lucterres@users.noreply.github.com>
2024-08-30 18:27:03 -0300

    locals inHouseAMD

--- suport/patchesMethods.py
+++ suport/patchesMethods.py
@@ -19,6 +19,14 @@ def rotateImage(img, angle):
 def loadDataBase(samples=200, treshold=100):
     #define localização dos diretórios de imagens
   
+    if locals.inHouseAMD:
+        #Desktop I3
+        housepath=r'D:\_0Luciano\_0PHD\datasets\tgs-sal'
+        TRAIN_CSV = housepath + r'\train1090.csv'
+        IMAGES_DIR = housepath + r'\train\images'
+        MASK_DIR = housepath + r'\masks10-90'
+
+
     if locals.inHouse:
         #Desktop I3
         housepath=r'D:\_0Luciano\_0PHD\datasets\tgs-sal'
@@ -26,6 +34,13 @@ def loadDataBase(samples=200, treshold=100):
         IMAGES_DIR = housepath + r'\train\images'
         MASK_DIR = housepath + r'\masks10-90'
     
+    if locals.inHouse:
+        #Desktop I3
+        housepath=r'D:\_0Luciano\_0PHD\datasets\tgs-sal'
+        TRAIN_CSV = housepath + r'\train1090.csv'
+        IMAGES_DIR = housepath + r'\train\images'
+        MASK_DIR = housepath + r'\masks10-90'
+
     if locals.inNote:
         notePath = r'D:\\_phd\datasets\\tgsSalt\\'
         TRAIN_CSV = notePath + r'train1090.csv'
